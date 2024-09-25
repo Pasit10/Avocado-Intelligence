49abe326e4dffcacaeb241b178a547caeb329d92
Pasit10 <pasit1006@gmail.com>
2024-09-25 23:22:27 +0700

    add requirement.txt

--- backend/routes/customer/model.py
+++ backend/routes/customer/model.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String
+
 from config.database import Base
 
 class Customer(Base):
-    __tablename__ = "customer_transaction"
+    __tablename__ = "customer"
 
     customer_id = Column(Integer, primary_key=True, index=True)
     sex = Column(String[255])
