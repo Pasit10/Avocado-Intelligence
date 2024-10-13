45f9c2b14a4642b42c48a2d11c8dbcea484f0d8d
Pasit10 <pasit1006@gmail.com>
2024-10-14 00:37:39 +0700

    load model gender or sex

--- backend/constant/constants.py
+++ backend/constant/constants.py
@@ -3,10 +3,13 @@
 # DATABASE CONFIG
 DATABASE_NAME = 'Ethnicity_Detection_Model'
 DATABASE_PORT = 3306
-USERNAME = 'username'
-PASSWORD = 'password' # database password
+USERNAME = 'root'
+PASSWORD = '0912263056' # database password
 
 # PATH TO MODEL
+PATH_MODEL_SEX = "model/model_gender.h5"
+PATH_WEIGHTS_SEX = "model/model_gender.weights.h5"
+
 PATH_MODEL_AGE = "model/model_age.h5"
 PATH_WEIGHTS_AGE = "model/model_age.h5.weights.h5"
 
