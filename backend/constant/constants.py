701e78917c95b8f5de37e98ae00e66c4309d370c
Pasit10 <pasit1006@gmail.com>
2024-10-15 10:10:37 +0700

    change file to download model

--- backend/constant/constants.py
+++ backend/constant/constants.py
@@ -3,15 +3,15 @@
 # DATABASE CONFIG
 DATABASE_NAME = 'Ethnicity_Detection_Model'
 DATABASE_PORT = 3306
-USERNAME = 'root'
-PASSWORD = '0912263056' # database password
+USERNAME = 'username'
+PASSWORD = 'password' # database password
 
 # PATH TO MODEL
-PATH_MODEL_SEX = "model/model_gender.h5"
-PATH_WEIGHTS_SEX = "model/model_gender.weights.h5"
+PATH_MODEL_SEX = "model/Best_Gender_Detection_Resnet50.h5"
+# PATH_WEIGHTS_SEX = "model/model_gender.weights.h5"
 
-PATH_MODEL_AGE = "model/model_age.h5"
-PATH_WEIGHTS_AGE = "model/model_age.h5.weights.h5"
+PATH_MODEL_AGE = "model/Best_Age_Detection_Resnet50.h5"
+# PATH_WEIGHTS_AGE = "model/model_age.h5.weights.h5"
 
-PATH_MODEL_RACE = "model/model_by_dear_race.json"
-PATH_WEIGHTS_RACE = "model/model_by_dear_race_weights.weights.h5"
+PATH_MODEL_RACE = "model/best_model_fold5.keras"
+# PATH_WEIGHTS_RACE = "model/model_by_dear_race_weights.weights.h5"
